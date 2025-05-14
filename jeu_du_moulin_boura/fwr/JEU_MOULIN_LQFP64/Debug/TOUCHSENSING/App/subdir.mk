ffd03f12a0b3c88b46bdc5309ee07fd6472f2bb9
boura01 <alexandre.bourquenoud@studentfr.ch>
2025-05-14 08:05:14 +0200

    changement du schéma Altium
    changement de plusieurs éléments sur altium ce qui nous rapproche de la version "fonctionnel" (en théorie)

--- jeu_du_moulin_boura/fwr/JEU_MOULIN_LQFP64/Debug/TOUCHSENSING/App/subdir.mk
+++ jeu_du_moulin_boura/fwr/JEU_MOULIN_LQFP64/Debug/TOUCHSENSING/App/subdir.mk
@@ -22,7 +22,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 TOUCHSENSING/App/%.o TOUCHSENSING/App/%.su TOUCHSENSING/App/%.cyclo: ../TOUCHSENSING/App/%.c TOUCHSENSING/App/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L072xx -c -I../Core/Inc -I../Drivers/STM32L0xx_HAL_Driver/Inc -I../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L0xx/Include -I../Drivers/CMSIS/Include -I../TOUCHSENSING/App -I../Middlewares/ST/STM32_TouchSensing_Library/inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L072xx -c -I../Core/Inc -I../Drivers/STM32L0xx_HAL_Driver/Inc -I../Drivers/STM32L0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L0xx/Include -I../Drivers/CMSIS/Include -I../TOUCHSENSING/App -I../Middlewares/ST/STM32_TouchSensing_Library/inc -Og -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-TOUCHSENSING-2f-App
 
---
+++ jeu_du_moulin_boura/pcb/lib/lib/readme.md
@@ -0,0 +1,6 @@
+# Dossier "lib"
+
+ Contient le template pour la librairie d'empreintes avec la définition des couches et 
+la librairie des VIA et des PAD
+
+
