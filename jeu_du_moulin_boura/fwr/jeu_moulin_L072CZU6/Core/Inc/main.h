b27c0432791285b079c2223b1da2ce4177059e2a
boura01 <105719519+boura01@users.noreply.github.com>
2025-06-01 12:43:21 +0200

    programmation
    J'ai changer pas mal de code pour faire fonctionner les LEDs. J'ai encore des problèmes avec les  touches.

---
+++ jeu_du_moulin_boura/fwr/jeu_moulin_L072CZU6/Core/Inc/main.h
@@ -0,0 +1,121 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define CS_G1_Pin GPIO_PIN_0
+#define CS_G1_GPIO_Port GPIOA
+#define L1_Pin GPIO_PIN_1
+#define L1_GPIO_Port GPIOA
+#define C1_Pin GPIO_PIN_2
+#define C1_GPIO_Port GPIOA
+#define C2_Pin GPIO_PIN_3
+#define C2_GPIO_Port GPIOA
+#define CS_G2_Pin GPIO_PIN_4
+#define CS_G2_GPIO_Port GPIOA
+#define L8_Pin GPIO_PIN_5
+#define L8_GPIO_Port GPIOA
+#define L7_Pin GPIO_PIN_6
+#define L7_GPIO_Port GPIOA
+#define C1A7_Pin GPIO_PIN_7
+#define C1A7_GPIO_Port GPIOA
+#define CS_G4_Pin GPIO_PIN_0
+#define CS_G4_GPIO_Port GPIOB
+#define L6_Pin GPIO_PIN_1
+#define L6_GPIO_Port GPIOB
+#define L5_Pin GPIO_PIN_2
+#define L5_GPIO_Port GPIOB
+#define L5_LED_Pin GPIO_PIN_10
+#define L5_LED_GPIO_Port GPIOB
+#define L4_LED_Pin GPIO_PIN_11
+#define L4_LED_GPIO_Port GPIOB
+#define C1_LED_Pin GPIO_PIN_8
+#define C1_LED_GPIO_Port GPIOA
+#define CS_G3_Pin GPIO_PIN_9
+#define CS_G3_GPIO_Port GPIOA
+#define L4_Pin GPIO_PIN_10
+#define L4_GPIO_Port GPIOA
+#define L3_Pin GPIO_PIN_11
+#define L3_GPIO_Port GPIOA
+#define L2_Pin GPIO_PIN_12
+#define L2_GPIO_Port GPIOA
+#define L3_LED_Pin GPIO_PIN_15
+#define L3_LED_GPIO_Port GPIOA
+#define C3_LED_Pin GPIO_PIN_3
+#define C3_LED_GPIO_Port GPIOB
+#define L2_LED_Pin GPIO_PIN_4
+#define L2_LED_GPIO_Port GPIOB
+#define C2_LED_Pin GPIO_PIN_5
+#define C2_LED_GPIO_Port GPIOB
+#define L1_LED_Pin GPIO_PIN_6
+#define L1_LED_GPIO_Port GPIOB
+#define L8_LED_Pin GPIO_PIN_7
+#define L8_LED_GPIO_Port GPIOB
+#define L7_LED_Pin GPIO_PIN_8
+#define L7_LED_GPIO_Port GPIOB
+#define L6_LED_Pin GPIO_PIN_9
+#define L6_LED_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ jeu_du_moulin_boura/fwr/jeu_moulin_L072CZU6/Debug/Core/Startup/startup_stm32l072czux.d
@@ -0,0 +1,2 @@
+Core/Startup/startup_stm32l072czux.o: \
+ ../Core/Startup/startup_stm32l072czux.s
